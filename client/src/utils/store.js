5775b8797cfe8d6bad2e9935e7554c710d1b88f0
Jorge Viera <Jorge5psgt@gmail.com>
2021-07-13 12:50:10 -0400

    complete

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,4 +1,5 @@
 //TODO:
-// import { createStore } from 'redux';
-// import reducers from './reducers';
+import { createStore } from 'redux';
+import reducers from './reducers';
 // Create a default export of `createStore` that accepts an argument of `reducers`.
+export default createStore(reducers);
